adf3e183768f4cbe850c19bc91f83d4b2a5f9827
raphdcst <rdecostaire@gmail.com>
2024-06-02 20:56:25 +0200

    Changed "guild" table schema

--- src/tables/guild.ts
+++ src/tables/guild.ts
@@ -3,7 +3,7 @@ import { UUID } from "crypto"
 
 export interface Guild {
   _id: UUID
-  guildId: string
+  guildId: number
   prefix: string | null
 }
 
@@ -11,7 +11,7 @@ export default new app.Table<Guild>({
   name: "guild",
   setup: (table) => {
     table.uuid("_id", { primaryKey: true })
-    table.string("guildId").unique().notNullable()
+    table.bigInteger("guildId").unique().notNullable()
     table.string("prefix")
   },
 })
