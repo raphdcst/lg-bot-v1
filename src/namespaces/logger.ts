462df4c08a205c179fd71a23cc1188f919a430a1
raphdcst <rdecostaire@gmail.com>
2024-06-25 17:43:57 +0200

    Minor changes

--- src/namespaces/logger.ts
+++ src/namespaces/logger.ts
@@ -3,3 +3,4 @@ import * as app from "#app"
 export const interactionLogger = new app.Logger({ section: 'interaction'})
 export const createLogger = new app.Logger({ section: 'create'})
 export const fetchLogger = new app.Logger({ section: 'fetch'})
+export const deleteLogger = new app.Logger({ section: 'delete'})
